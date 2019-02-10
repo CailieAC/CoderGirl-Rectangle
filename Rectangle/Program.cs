350076c442ceb6ce673abd26a3527984e7c98fd9
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-09 22:10:24 -0600

    commit1

--- Rectangle/Program.cs
+++ Rectangle/Program.cs
@@ -9,13 +9,15 @@ namespace Rectangle
             Console.Write("Enter the length of the rectangle: ");
 
             // Get input from the user and save it to a variable.
+            float length = float.Parse(Console.ReadLine());
 
             Console.Write("Enter the width of the rectangle: ");
 
             // Get input from the user and save it to a second variable.
+            float width = float.Parse(Console.ReadLine());
 
             // Output - The area of the rectangle is (computed value)
-
+            Console.WriteLine("The area of the rectangle is " + length*width);
             Console.ReadLine();
         }
     }
